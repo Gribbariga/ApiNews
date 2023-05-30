ab0ef4a7adbd4fc78c4303561992a3d7de841d38
Gribbariga <mormir09@gmail.com>
2023-05-30 22:18:43 +0300

    add delete news

--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -6,5 +6,6 @@ const checkRole = require("../middleware/checkRoleMiddleware");
 router.post("/", newsController.postNews);
 router.get("/", newsController.getNews);
 router.get("/:id", newsController.getById);
+router.post("/delete", newsController.deleteNews);
 
 module.exports = router;
